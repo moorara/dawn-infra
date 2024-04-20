b4dd968f091cd2d0b7925c4b5788b24bacdc5d76
Milad <moorara@users.noreply.github.com>
2024-04-20 15:38:28 -0400

    Housekeeping updates (#7)

--- x-ui/aws/versions.tf
+++ x-ui/aws/versions.tf
@@ -3,18 +3,18 @@
 
 terraform {
   # Root modules should constraint both a lower and upper bound on versions for each provider.
-  required_version = "~> 1.3"
+  required_version = "~> 1.8"
 
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/local/latest/docs
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.2"
+      version = "~> 2.5"
     }
     # https://registry.terraform.io/providers/hashicorp/random/latest/docs
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.4"
+      version = "~> 3.6"
     }
     # https://registry.terraform.io/providers/hashicorp/tls/latest/docs
     tls = {
@@ -24,17 +24,17 @@ terraform {
     # https://registry.terraform.io/providers/vancluever/acme/latest/docs
     acme = {
       source  = "vancluever/acme"
-      version = "~> 2.12"
+      version = "~> 2.21"
     }
     # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = "~> 4.30"
     }
     # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.46"
     }
   }
 }
