925505153d2c4ec6581f0c367a21ed8976d85704
Milad <moorara@users.noreply.github.com>
2025-06-25 15:05:35 -0400

    Update Terraform provider versions (#11)

--- x-ui/aws/dns.tf
+++ x-ui/aws/dns.tf
@@ -5,14 +5,16 @@ provider "cloudflare" {
 
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/data-sources/zone
 data "cloudflare_zone" "domain" {
-  name = var.domain
+  filter = {
+    name = var.domain
+  }
 }
 
-# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
-resource "cloudflare_record" "www_ipv4" {
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
+resource "cloudflare_dns_record" "www_ipv4" {
   zone_id = data.cloudflare_zone.domain.id
-  type    = "A"
   name    = local.subdomain
-  value   = aws_instance.vpn.public_ip
+  type    = "A"
+  content = aws_instance.vpn.public_ip
   ttl     = 1800
 }
--- x-ui/vultr/dns.tf
+++ x-ui/vultr/dns.tf
@@ -5,23 +5,25 @@ provider "cloudflare" {
 
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/data-sources/zone
 data "cloudflare_zone" "domain" {
-  name = var.domain
+  filter = {
+    name = var.domain
+  }
 }
 
-# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
-resource "cloudflare_record" "www_ipv4" {
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
+resource "cloudflare_dns_record" "www_ipv4" {
   zone_id = data.cloudflare_zone.domain.id
-  type    = "A"
   name    = local.subdomain
-  value   = vultr_instance.server.main_ip
+  type    = "A"
+  content = vultr_instance.server.main_ip
   ttl     = 1800
 }
 
-# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
-resource "cloudflare_record" "www_ipv6" {
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
+resource "cloudflare_dns_record" "www_ipv6" {
   zone_id = data.cloudflare_zone.domain.id
-  type    = "AAAA"
   name    = local.subdomain
-  value   = vultr_instance.server.v6_main_ip
+  type    = "AAAA"
+  content = vultr_instance.server.v6_main_ip
   ttl     = 1800
 }
